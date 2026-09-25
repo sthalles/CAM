4a8718ce95086de8f72070935ac2ea3663337c7d
Thalles Silva <thalles753@gmail.com>
2026-09-25 19:24:15 -0300

    Fix: Change distributed backend from Gloo to NCCL to support ReduceOp.AVG

--- cam/distributed/torch_distributed_wrapper.py
+++ cam/distributed/torch_distributed_wrapper.py
@@ -261,7 +261,7 @@ def enable_distributed(
     if set_cuda_current_device:
         torch.cuda.set_device(torch_env.local_rank)
 
-    dist.init_process_group(backend="gloo", timeout=timeout)
+    dist.init_process_group(backend="nccl", timeout=timeout)
     dist.barrier(device_ids=[torch_env.local_rank])
 
     if restrict_print_to_main_process:
